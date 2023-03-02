b0df8ed95dd7e3d9dd7c5eb090da35149b1235db
serdarferhat <cylnsrdrr@gmail.com>
2023-03-02 09:54:15 +0300

    detay_css

--- src/pages/BookDetail.js
+++ src/pages/BookDetail.js
@@ -24,12 +24,48 @@ useEffect(()=>{
   return (
     <>
     <Header/>
-    <h1>Kitap Adı:{book?.title}</h1>
+    {/* <h1>Kitap Adı:{book?.title}</h1>
     <h1>Yazarı:{book?.author}</h1>
     <h1>Yayın evi:{book?.publisher}</h1>
     <h1>ISBN:{book?.isbn===""?"-":book?.isbn}</h1>
     <h1>Durumu:{book?.isRead===true?"Okundu":"Okunmadı"}</h1>
-    <h1>Kategori:{category?.name}</h1>
+    <h1>Kategori:{category?.name}</h1> */}
+    <h3 className='text-center mt-4'>Kitap Detayı </h3>
+    <table class="container table table-bordered mt-4">
+      <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Kitap Adı</td>
+      <td>{book?.title}</td>   
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Yazarı</td>
+      <td>{book?.author}</td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td >Yayın evi</td>
+      <td>{book?.publisher}</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td >ISBN</td>
+      <td>{book?.isbn===""?"-":book?.isbn}</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td >Durumu</td>
+      <td>{book?.isRead===true?"Okundu":"Okunmadı"}</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td >Kategori</td>
+      <td>{category?.name}</td>
+    </tr>
+  </tbody>
+</table>
     </>
   )
 }
