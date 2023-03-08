9df9ff0c245f15b449af35c04d63ec6d5b463cdb
serdarferhat <cylnsrdrr@gmail.com>
2023-03-09 00:01:35 +0300

    modal_özelleştirme_error_success

--- src/pages/AddCategory.js
+++ src/pages/AddCategory.js
@@ -1,8 +1,8 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 import Header from '../components/Header'
 
-import { useSelector,useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import actionTypes from "../redux/actions/actionTypes"
 
 import { useNavigate } from 'react-router-dom'
@@ -11,63 +11,64 @@ import api from "../api/api"
 import urls from "../api/urls"
 
 const AddCategory = () => {
-  const navigate=useNavigate()
-  const dispatch=useDispatch()
-  const {categoriesState}=useSelector(state=>state)
-  const [form,setForm]=useState({
-    id:String(new Date().getTime()),
-    name:""
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { categoriesState } = useSelector(state => state)
+  const [form, setForm] = useState({
+    id: String(new Date().getTime()),
+    name: ""
   })
-  const handleSubmit=(event)=>{
+  const handleSubmit = (event) => {
     event.preventDefault()
     // kategori boş olamaz ----değer verirken
     // daha önce kayıtlı kategori kayıt edilemez 
-    if(form.name===""){ alert( "Kitabınızın Kategorisini Giriniz!!!")
-     return
+    if (form.name === "") {
+      alert("Kitabınızın Kategorisini Giriniz!!!")
+      return
     }
-   const hasCategory=categoriesState.categories.find(item=>item.name.toLocaleLowerCase("tr-TR")===form.name.toLocaleLowerCase("tr-TR"));
-   console.log(hasCategory)
-   if(hasCategory!==undefined){
-    alert(`${form.name} adıyla kitap kategorisi zaten vardır..`)
-    return
-   }
-   api.post(urls.categories,form)
-   .then(res=>{
-    dispatch({type:actionTypes.categoryTypes.ADD_CATEGORY,payload:form})
-    navigate("/list-categories")
-   })
+    const hasCategory = categoriesState.categories.find(item => item.name.toLocaleLowerCase("tr-TR") === form.name.toLocaleLowerCase("tr-TR"));
+    console.log(hasCategory)
+    if (hasCategory !== undefined) {
+      alert(`${form.name} adıyla kitap kategorisi zaten vardır..`)
+      return
     }
+    api.post(urls.categories, form)
+      .then(res => {
+        dispatch({ type: actionTypes.categoryTypes.ADD_CATEGORY, payload: form })
+        navigate("/list-categories")
+      })
+  }
 
 
 
   return (
     <div>
-     <Header/>
+      <Header />
       <div className='container mt-4'>
-       <form onSubmit={handleSubmit}>
-       <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Kategori Adı<span style={{ color: "orangered" }}>*</span>
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            placeholder="Roman.."
-            value={form.name}
-            onChange={(event) =>
-              setForm({ ...form, name: event.target.value })
-            }
-          />
-           <div className="d-flex justify-content-center my-5">
-          <button className="btn btn-secondary w-50" type="submit">
-            Kaydet
-          </button>
-        </div>
-        </div>
-       </form>
-                </div>
-                </div>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Kategori Adı<span style={{ color: "orangered" }}>*</span>
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Roman.."
+              value={form.name}
+              onChange={(event) =>
+                setForm({ ...form, name: event.target.value })
+              }
+            />
+            <div className="d-flex justify-content-center my-5">
+              <button className="btn btn-secondary w-50" type="submit">
+                Kaydet
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- src/pages/EditCategory.js
+++ src/pages/EditCategory.js
@@ -3,68 +3,111 @@ import React, { useState } from 'react'
 import Header from '../components/Header'
 
 import { useParams } from 'react-router-dom'
-import { useSelector,useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { firstBig } from '../utils/firstBig'
 import { useNavigate } from 'react-router-dom'
 
 import api from "../api/api"
 import urls from "../api/urls"
 import actionTypes from '../redux/actions/actionTypes'
+import GeneralModal from "../components/GeneralModal";
 
 
 const EditCategory = () => {
-  const navigate=useNavigate()
-  const dispatch=useDispatch()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const { categoryId } = useParams()
   // console.log(categoryId)
   const { categoriesState } = useSelector(state => state)
   const myCategory = categoriesState.categories.find((item) => item.id === categoryId)
   // console.log(myCategory)
-  const [form,setForm]=useState(myCategory)
-  const handleEdit=(e)=>{
+  const [form, setForm] = useState(myCategory)
+  const [showModal, setShowModal] = useState(false)
+  const [modalFeature, setModalFeature] = useState({
+    show: false,
+    message: "",
+    title: "",
+    type: "error"
+  })
+
+
+
+
+  const handleEdit = (e) => {
     e.preventDefault()
-    if(!form.name){
-      alert("Kategori adı boş olamaz")
+    if (!form.name) {
+     setModalFeature({
+      show:true,
+      message:"Kategori adı boş bırakılamaz..",
+      title:"Hata",
+      type:"error"
+     })
       return
     }
-    const hasCategory=categoriesState.categories.find(item=>firstBig(item.name)===firstBig(form.name));
-    if(hasCategory){
-      alert(`${form.name} adıyla kayıtlı kategori vardır.`);
+    const hasCategory = categoriesState.categories.find(item => firstBig(item.name) === firstBig(form.name));
+    if (hasCategory) {
+      setModalFeature({
+        show:true,
+        message:`${form.name} adıyla kayıtlı kategori vardır.`,
+        title:"Hata",
+        type:"error"
+      })
       return
     }
-    api.put(`${urls.categories}/${categoryId}`,form)
-    .then(res=>{
-      dispatch({type:actionTypes.categoryTypes.EDIT_CATEGORY,payload:{...form,name:firstBig(form.name).trim()}})
-      navigate("/list-categories")
-    })
-    .catch(err=>{})
+    api.put(`${urls.categories}/${categoryId}`, form)
+      .then(res => {
+        dispatch({ type: actionTypes.categoryTypes.EDIT_CATEGORY, payload: { ...form, name: firstBig(form.name).trim() } })
+        setModalFeature({
+          show:true,
+          title:"Başarılı",
+          message:"Kategori güncelleme işlemi başarıyla gerçekleşti..",
+          type:"ok"
+        })
+      })
+      .catch(err => { })
   }
   return (
     <div>
       <Header />
       .<div className="container">
-      <form onSubmit={handleEdit}>
-       <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Kategori Adı<span style={{ color: "orangered" }}>*</span>
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            placeholder="Roman.."
-            value={form.name}
-            onChange={(e)=>setForm({...form,name:e.target.value})}
-          
-          />
-           <div className="d-flex justify-content-center my-5">
-          <button disabled={firstBig(myCategory.name)===firstBig(form.name).trim()} className="btn btn-secondary w-50" type="submit">
-            Güncelle
-          </button>
-        </div>
-        </div>
-       </form>
+        <form onSubmit={handleEdit}>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Kategori Adı<span style={{ color: "orangered" }}>*</span>
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Roman.."
+              value={form.name}
+              onChange={(e) => setForm({ ...form, name: e.target.value })}
+
+            />
+            <div className="d-flex justify-content-center my-5">
+              <button disabled={firstBig(myCategory.name) === firstBig(form.name).trim()} className="btn btn-secondary w-50" type="submit">
+                Güncelle
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
+      {
+        modalFeature.show &&(
+          <GeneralModal 
+          title={modalFeature.title} 
+          content={modalFeature.message} 
+          buttonText={modalFeature.type==="error"?"Kapat":"Katogarilere Dön"}
+          buttonOnClick={()=>{
+          if(modalFeature.type === "error"){
+            setModalFeature({...modalFeature,show:false})
+          }else{
+            navigate("/list-categories")
+          }
+        }}/>
+        )
+
+      }
     </div>
   )
 }
